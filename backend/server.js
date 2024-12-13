237a98c4d5f922e0a212b7b779da754a57fc8619
PavLouis <louis.pavlovsky@epitech.eu>
2024-12-13 17:33:29 +0100

    [ADD]: Swagger configured /docs -> for now just createWallet and login added

--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,23 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
-
-const app = express();
-app.use(cors());
-app.use(express.json());
+const swaggerUI = require('swagger-ui-express')
+const YAML = require('yamljs')
+const swaggerDoc = YAML.load('./swagger.yaml')
 
 const authRoutes = require('./routes/authentification');
 const rwaRoutes = require('./routes/rwa');
 
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+
 app.use('/api/auth', authRoutes);
 app.use('/api/rwa', rwaRoutes);
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDoc))
 
 
 // Start server
