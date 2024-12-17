98d1b2e86b589cb3708f112e11695fa32865c6c2
bill.on@epitech.eu <bill.on@epitech.eu>
2024-12-17 18:06:08 +0100

    [TEST] print all env

--- backend/config/mgdb.js
+++ backend/config/mgdb.js
@@ -3,6 +3,9 @@ require('dotenv').config();
 
 async function connectDatabase() {
     try {
+        console.log("Environment Variables:");
+        console.log(JSON.stringify(process.env, null, 2));
+
         const dburl = process.env.MONGODB_URI || "mongodb://mongo:27017/blockchain-database-mongo";
 
         console.log(`DB URI: ${dburl}`);
